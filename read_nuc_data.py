f7ef3b40b1f44eae321de678799ed20997efbfdd
Enrico Raphael Skoruppa <esdrifter@gmail.com>
2024-02-11 18:37:19 +0100

    initial commit

--- read_nuc_data.py
+++ read_nuc_data.py
@@ -1,11 +1,7 @@
 import sys, os, glob
 import numpy as np
 from typing import List, Tuple, Callable, Any, Dict
-from .PolyCG.polycg.transform_algebra2group import algebra2group_stiffmat
-from .PolyCG.polycg.transform_midstep2triad import midstep2triad
-from .PolyCG.polycg.SO3 import so3
-
-
+from .SO3 import so3
 
 class GenStiffness:
     
@@ -20,14 +16,12 @@ class GenStiffness:
             path = os.path.join(os.path.dirname(__file__), 'Parametrization/Crystallography')
         else:
             raise ValueError(f'Unknown method "{self.method}".')
-        
         bases = 'ATCG'
         self.dimers = {}
         for b1 in bases:
             for b2 in bases:
                 seq = b1+b2
                 self.dimers[seq] = self.read_dimer(seq,path)
-                
             
     def read_dimer(self, seq: str, path: str):
         fnstiff = glob.glob(path+'/Stiffness*'+seq+'*')[0]
@@ -35,8 +29,8 @@ class GenStiffness:
         
         equi = np.loadtxt(fnequi)
         stiff = np.loadtxt(fnstiff)
-        equi_triad = midstep2triad(equi)
-        stiff_group = algebra2group_stiffmat(np.array([equi_triad]),stiff)
+        equi_triad = so3.se3_midstep2triad(equi)                
+        stiff_group = so3.se3_algebra2group_stiffmat(equi,stiff,translation_as_midstep=True)        
         dimer = {
             'seq' : seq,
             'group_gs':   equi_triad,
@@ -46,7 +40,7 @@ class GenStiffness:
             }
         return dimer
     
-    def gen_params(self, seq: str, use_group: str=True):
+    def gen_params(self, seq: str, use_group: str=False):
         N = len(seq)-1
         stiff = np.zeros((6*N,6*N))
         gs    = np.zeros((N,6))
@@ -63,45 +57,21 @@ class GenStiffness:
             gs[i] = pgs
         return stiff,gs 
     
-def read_nucleosome_triads(fn):
-    data = np.loadtxt(fn)
-    N = len(data) // 12
-    nuctriads = np.zeros((N,4,4))
-    for i in range(N):
-        tau = np.eye(4)
-        pos   = data[i*12:i*12+3] / 10
-        triad = data[i*12+3:i*12+12].reshape((3,3))
-        triad = so3.euler2rotmat(so3.rotmat2euler(triad))
-        tau[:3,:3] = triad
-        tau[:3,3]  = pos
-        nuctriads[i] = tau
-    return nuctriads
-
-
-    
-
-        
-        
-        
-
 
 
 if __name__ == '__main__':
     
     np.set_printoptions(linewidth=250,precision=3,suppress=True)
+    method = sys.argv[1].upper()
+    genstiff = GenStiffness(method=method)
     
-    # genstiff = GenStiffness(method='MD')
+    seq = 'ATCG'
     
-    # seq = ''.join(['ATCG'[np.random.randint(4)] for i in range(147)])
-    # stiff,gs = genstiff.gen_params(seq)
-
-    # print(gs)
-    # print(stiff)
+    genstiff.gen_params(seq,use_group=True)
+    
     
     triadfn = os.path.join(os.path.dirname(__file__), 'State/Nucleosome.state')
-    
     nuctriads = read_nucleosome_triads(triadfn)
-    
     N = len(nuctriads)
 
     for i in range(N-1):
